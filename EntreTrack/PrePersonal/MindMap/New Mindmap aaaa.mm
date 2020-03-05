f5e8defea680cc3f5465b873a46bc3a80a1164e9
Joel Robert Justiawan <joelwindows7@gmail.com>
2020-03-05 11:49:59 +0700

    susah
    susah boi

---
+++ EntreTrack/PrePersonal/MindMap/New Mindmap aaaa.mm
@@ -0,0 +1,56 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1583129816307" ID="ID_144387358" MODIFIED="1583129839658" TEXT="New Mindmap aaaa">
+<icon BUILTIN="messagebox_warning"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1583129895748" ID="ID_1639934803" MODIFIED="1583129900041" POSITION="right" TEXT="bre"/>
+<node CREATED="1583129911685" ID="ID_887019668" MODIFIED="1583129915419" POSITION="left" TEXT="faaa">
+<node CREATED="1583129916939" ID="ID_1933093729" MODIFIED="1583129957644" TEXT="aaa">
+<node CREATED="1583129919593" HGAP="62" ID="ID_890069567" MODIFIED="1583129974701" TEXT="aaa" VSHIFT="190">
+<node CREATED="1583129978531" ID="ID_1439565561" MODIFIED="1583129982913" TEXT="sssfe"/>
+</node>
+</node>
+</node>
+<node CREATED="1583129924170" ID="ID_1750762709" MODIFIED="1583129925815" POSITION="right" TEXT="sss"/>
+<node CREATED="1583129928931" ID="ID_1865391878" MODIFIED="1583130144219" POSITION="left" TEXT="ssss">
+<cloud/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1583129941966" ID="ID_965192026" MODIFIED="1583130044490" TEXT="ssss">
+<icon BUILTIN="full-9"/>
+</node>
+<node CREATED="1583129947337" ID="ID_1672862294" MODIFIED="1583130046907" TEXT="ssss">
+<icon BUILTIN="full-8"/>
+</node>
+<node CREATED="1583129950216" ID="ID_662191181" MODIFIED="1583130116421">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Segoe UI">ssss</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-7"/>
+<icon BUILTIN="full-4"/>
+<icon BUILTIN="full-7"/>
+<icon BUILTIN="full-9"/>
+<icon BUILTIN="full-0"/>
+<icon BUILTIN="full-6"/>
+<icon BUILTIN="ksmiletris"/>
+<icon BUILTIN="attach"/>
+<icon BUILTIN="down"/>
+<icon BUILTIN="up"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="back"/>
+<icon BUILTIN="clanbomber"/>
+<attribute NAME="h" VALUE="u"/>
+</node>
+<node CREATED="1583130129236" ID="ID_426682824" MODIFIED="1583130140914" TEXT="klo">
+<cloud/>
+</node>
+</node>
+</node>
+</map>
